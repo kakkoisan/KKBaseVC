ee4a234d664fefb66827fe589ad6cb38a1d2c427
张晓飞 <zhangxiaofei01@qiyi.com>
2019-11-14 21:40:47 +0800

    Initial Commit

---
+++ KKBaseVC/KKBaseVC.h
@@ -0,0 +1,19 @@
+//
+//  KKBaseVC.h
+//  KKBaseVC
+//
+//  Created by 干啥啥不行 吃饭第一名🥇 on 2019/11/14.
+//  Copyright © 2019 干啥啥不行 吃饭第一名🥇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KKBaseVC.
+FOUNDATION_EXPORT double KKBaseVCVersionNumber;
+
+//! Project version string for KKBaseVC.
+FOUNDATION_EXPORT const unsigned char KKBaseVCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKBaseVC/PublicHeader.h>
+
+
